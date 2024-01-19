9d83ff1a6613618ad4eb895158dde5ce8f910f40
ceslo <slovesce@gmail.com>
2024-01-19 12:30:55 +0100

    index avec bootstrap, jointure à terminer

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ catch (PDOException $e){
     echo "Erreur: ".$e->getMessage() . "<br>";
     echo "N° : " .$e->getCode();
     die("Fin du script");};
-$disc= $db-> prepare("SELECT * FROM disc");
-$disc= $db-> execute();
+$disc= $db-> prepare("SELECT * FROM disc JOIN artist on disc.artist_id = artist.artist_id");
+$disc-> execute();
 $result= $disc-> fetchAll();
 ?>
 
@@ -16,12 +16,52 @@ $result= $disc-> fetchAll();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+      crossorigin="anonymous"
+    />
     <title>Index Record</title>
-    <?php
-     foreach($disc as $album){
-        }   ?>
-</head>
+    </head>
 <body>
-    
+<h1> Liste des disques</h1>
+<br> 
+      <div class="row row-cols-3"> 
+    <?php
+     foreach($result as $album){
+      // var_dump($album);
+     echo 
+'<div class ="card mb-3">
+    <div class="row"> 
+      <div class="col">
+          <img style="max-width: 250px;" src="pictures/'.$album["disc_picture"].'"> 
+      </div>
+      <div class="col">
+        <div class="card-body">
+          <div class="card-title fw-bold fs-4" >'.$album["disc_title"].'
+          </div>
+          <div class="card-text text-start"> 
+          Artiste'' 
+          <br>
+          Label: '.$album["disc_label"].'
+          <br> 
+          Year: '.$album["disc_year"].'
+          <br> 
+          Genre: '.$album["disc_genre"].'
+          </div>
+        </div>
+        </div> 
+    </div>    
+</div>';
+        }   
+        ?>
+        </div>
+<script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+      crossorigin="anonymous"
+    ></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 </body>
 </html>
